d21332b32c5ded61b143b6074833679f6d02bad2
Weslei Casali <wesleicasali18@gmail.com>
2022-11-24 01:38:21 -0300

    att

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Troca de foto banda
 setInterval(() => {
    
     var img = document.querySelector('#info-right > img')
@@ -11,3 +12,67 @@ setInterval(() => {
       }
 
 }, 4000);
+
+
+
+//Animação Select integrante
+function animaSelect(){
+       setTimeout(() => {
+            document.getElementById('img1').classList.add('selectImage')
+       }, 300);
+
+       setTimeout(() => {
+          document.getElementById('img1').classList.remove('selectImage')
+          document.getElementById('img2').classList.add('selectImage')
+       }, 600);
+
+       setTimeout(() => {
+          document.getElementById('img2').classList.remove('selectImage')
+          document.getElementById('img3').classList.add('selectImage')
+       }, 800);
+
+       setTimeout(() => {
+          document.getElementById('img3').classList.remove('selectImage')
+          document.getElementById('img4').classList.add('selectImage')
+       }, 1000);
+       
+       setTimeout(() => {
+          document.getElementById('img4').classList.remove('selectImage')
+       }, 1200);
+ 
+}
+
+
+
+
+//Oculta seleção de integrante e Mostra o integrante
+document.getElementById('img1').addEventListener('click', ()=>{
+        document.getElementById('index-select').style.display = 'none'
+        document.getElementById('i-1').style.display = 'flex'
+})
+
+document.getElementById('img2').addEventListener('click', ()=>{
+        document.getElementById('index-select').style.display = 'none'
+        document.getElementById('i-2').style.display = 'flex'
+})
+
+document.getElementById('img3').addEventListener('click', ()=>{
+        document.getElementById('index-select').style.display = 'none'
+        document.getElementById('i-3').style.display = 'flex'
+})
+
+document.getElementById('img4').addEventListener('click', ()=>{
+        document.getElementById('index-select').style.display = 'none'
+        document.getElementById('i-4').style.display = 'flex'
+})
+
+
+//FUNÇÃO PARA RETORNAR APÓS SELEÇÃO DO INTEGRANTE
+function back(){
+      document.getElementById('i-1').style.display = 'none'
+      document.getElementById('i-2').style.display = 'none'
+      document.getElementById('i-3').style.display = 'none'
+      document.getElementById('i-4').style.display = 'none'
+
+      document.getElementById('index-select').style.display = 'flex'
+}
